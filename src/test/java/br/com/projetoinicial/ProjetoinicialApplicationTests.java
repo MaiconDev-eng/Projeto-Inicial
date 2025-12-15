11a5b7cccfc6430c1d58d074125d6f84a9206ec5
Maicon <maicondx25@gmail.com>
2025-12-14 23:32:20 -0300

    first commit

---
+++ src/test/java/br/com/projetoinicial/ProjetoinicialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetoinicial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoinicialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
